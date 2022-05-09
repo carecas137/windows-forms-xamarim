f2f241c5dbd2d8c74f73eba207d4b555f45e7ba9
Carecas <60648131+champsilva16@users.noreply.github.com>
2022-05-09 19:44:01 +0100

    09/05/2022

---
+++ 14949_5/14949_5/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace _14949_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inserir = new System.Windows.Forms.TextBox();
+            this.btn_inserir = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inserir
+            // 
+            this.inserir.Location = new System.Drawing.Point(40, 53);
+            this.inserir.Name = "inserir";
+            this.inserir.Size = new System.Drawing.Size(126, 20);
+            this.inserir.TabIndex = 0;
+            this.inserir.TextChanged += new System.EventHandler(this.inserir_TextChanged);
+            // 
+            // btn_inserir
+            // 
+            this.btn_inserir.Location = new System.Drawing.Point(186, 53);
+            this.btn_inserir.Name = "btn_inserir";
+            this.btn_inserir.Size = new System.Drawing.Size(75, 23);
+            this.btn_inserir.TabIndex = 1;
+            this.btn_inserir.Text = "Inserir";
+            this.btn_inserir.UseVisualStyleBackColor = true;
+            this.btn_inserir.Click += new System.EventHandler(this.btn_inserir_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col1,
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(338, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(378, 343);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ISBN";
+            // 
+            // col1
+            // 
+            this.col1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.col1.HeaderText = "ISBN";
+            this.col1.MinimumWidth = 10;
+            this.col1.Name = "col1";
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Estado";
+            this.Column1.MinimumWidth = 10;
+            this.Column1.Name = "Column1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_inserir);
+            this.Controls.Add(this.inserir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inserir;
+        private System.Windows.Forms.Button btn_inserir;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
+
---
+++ 14949_5/14949_5/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace _14949_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void inserir_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string Fich = "digito.xml";
+            dataGridView1.AllowUserToAddRows = false;
+            if (File.Exists(Fich) == false)
+            {
+                XmlTextWriter FicheiroXml = new XmlTextWriter(Fich, System.Text.Encoding.UTF8);
+                FicheiroXml.WriteStartDocument(true);
+                FicheiroXml.Formatting = Formatting.Indented;
+                FicheiroXml.Indentation = 2;
+                FicheiroXml.WriteStartElement("isbn13");
+                FicheiroXml.WriteEndDocument();
+                FicheiroXml.Close();
+            }
+            testar();
+
+        }
+
+        private void btn_inserir_Click(object sender, EventArgs e)
+        {
+
+            int teste = 0;
+
+            bool success2 = int.TryParse(inserir.Text, out teste);
+            if (inserir.Text != "" && inserir.TextLength==13)
+            {
+                
+                string Fich = "digito.xml";
+                XmlDocument Ficheiroxml = new XmlDocument();
+                Ficheiroxml.Load(Fich);
+                XmlElement isbn = Ficheiroxml.CreateElement("isbn");
+                XmlAttribute numero = Ficheiroxml.CreateAttribute("numero");
+                numero.InnerText = inserir.Text;
+                isbn.Attributes.Append(numero);
+                Ficheiroxml.DocumentElement.AppendChild(isbn);
+                Ficheiroxml.Save(Fich);
+                MessageBox.Show("Inserido");
+            }
+            else
+            {
+                MessageBox.Show("Numero nao valido");
+            }
+            testar();
+        }
+        void testar()
+        {
+            string Fich = "digito.xml";
+            if (File.Exists(Fich) == true)
+            {
+
+                XmlDocument FicheiroXML = new XmlDocument();
+
+                FicheiroXML.Load(Fich);
+                dataGridView1.Rows.Clear();
+                foreach (XmlNode no in FicheiroXML.SelectNodes("//isbn"))
+                {
+                    int real;
+                    int real1=0;
+                    string n = no.Attributes[0].Value;
+                    string last = n.Substring(n.Length - 1);
+
+                  
+                    //numero final
+                    bool success = int.TryParse(last, out real);
+                    //soma dos 12 
+                    string so12 = n.Remove(n.Length - 1);
+                    int soma = 0;
+                    for(int i=0; i < so12.Length; i++)
+                    {
+                        char first = so12[i];
+                        real1 = first - '0';
+                        if(i==0 || i % 2 == 0)
+                        {
+                           
+                            real1 = real1 * 1;
+                            soma = soma + real1;
+                            
+                        }
+                        else
+                        {
+                            real1 = real1 * 3;
+                            soma = soma + real1;
+                            
+                        }
+
+                    }
+                   
+                    //calcular o resto 
+                    int resto = soma % 10;
+                    //passo 3
+                    int passo3 = 10 - resto;
+                    int control = 0;
+                    if (passo3 == 10)
+                    {
+                         control = 0;
+                    }
+                    else
+                    {
+                         control = passo3;
+
+                    }
+                    string estado;
+                    if (real == control)
+                    {
+                         estado = "OK";
+                    }
+                    else
+                    {
+                         estado = "ERRO";
+                    }
+
+                    dataGridView1.Rows.Add(no.Attributes[0].Value,estado);
+
+
+
+                }
+            }
+        }
+    }
+}
